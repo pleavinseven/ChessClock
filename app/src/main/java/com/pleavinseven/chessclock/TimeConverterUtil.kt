dc25848e98f16e85a75fa784b6a2c1e455a567ee
pleavin <pleavinseven@gmail.com>
2023-08-31 17:10:45 +0200

    updated formatTime to work show hours and milliseconds.

--- app/src/main/java/com/pleavinseven/chessclock/TimeConverterUtil.kt
+++ app/src/main/java/com/pleavinseven/chessclock/TimeConverterUtil.kt
@@ -1,11 +1,22 @@
 package com.pleavinseven.chessclock
 
-import android.text.format.DateUtils
+import java.time.Duration
 
 class TimeConverterUtil {
 
     fun formatTime(timeMillis: Long): String {
-        return DateUtils.formatElapsedTime(timeMillis / 1000)
+        var timeLeft = Duration.ofMillis(timeMillis)
+        val hours = timeLeft.toHours()
+        timeLeft = timeLeft.minusHours(hours)
+        val minutes = timeLeft.toMinutes()
+        timeLeft = timeLeft.minusMinutes(minutes)
+        val seconds = timeLeft.seconds
+        timeLeft = timeLeft.minusSeconds(seconds)
+        val millis = timeLeft.toMillis() / 10
+        return String.format(
+            "%02d:%02d:%02d:%02d",
+            hours, minutes, seconds, millis
+        )
     }
 
 }
